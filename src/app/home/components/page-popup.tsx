bd1bb38e8569b919f533a249671a7372b4d57f3a
GlebChiz <gchyz@jsdaddy.io>
2024-04-18 08:43:43 +0300

    feat: page popup

--- src/app/home/components/page-popup.tsx
+++ src/app/home/components/page-popup.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { ChangeEvent, useState } from 'react';
+import { IListItem } from '@/shared/types/page.types';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 export default function PagePopup() {
     const [formData, setFormData] = useState({
@@ -8,6 +9,17 @@ export default function PagePopup() {
         heading: '',
         topText: '',
         bottomText: '',
+        bold: '',
+        regular: '',
+    });
+    const [list, setList] = useState<IListItem[]>([]);
+    const [error, setErrors] = useState({
+        name: null as string | null,
+        heading: null as string | null,
+        topText: null as string | null,
+        bottomText: null as string | null,
+        bold: null as string | null,
+        regular: null as string | null,
     });
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -15,19 +27,65 @@ export default function PagePopup() {
             ...formData,
             [name]: value,
         });
+        if (!value) {
+            setErrors({
+                ...error,
+                [name]: 'Field is required',
+            });
+        } else {
+            setErrors({
+                ...error,
+                [name]: null,
+            });
+        }
+    };
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // eslint-disable-next-line no-unused-vars
+        const { bold, regular, ...rest } = formData;
+        console.log({ ...rest, list });
+    };
+    const addItem = (): void => {
+        setList([...list, { bold: formData.bold, regular: formData.regular }]);
+        setFormData({
+            ...formData,
+            bold: '',
+            regular: '',
+        });
+    };
+    const removeItem = (index: number): void => {
+        setList(list.filter((_it, i) => i !== index));
+    };
+    const disabledSaveButton = (): boolean => {
+        return (
+            !formData.name ||
+            !formData.heading ||
+            !formData.topText ||
+            !formData.bottomText ||
+            !list.length
+        );
     };
     return (
         <div className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-40 flex flex-row items-center justify-center">
-            <form className="p-16 flex flex-col gap-16 rounded-4 bg-white desktop:p-24 desktop:gap-16">
+            <form
+                onSubmit={handleSubmit}
+                className="p-16 flex flex-col gap-16 rounded-4 bg-white desktop:p-24 desktop:gap-16">
                 <div className="flex flex-col">
                     <label className="label">Name *</label>
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                    <input
+                        className={error.name ? 'invalid' : ''}
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="flex flex-col">
                     <label className="label">Heading *</label>
                     <input
+                        className={error.heading ? 'invalid' : ''}
                         type="text"
-                        name="name"
+                        name="heading"
                         value={formData.heading}
                         onChange={handleChange}
                     />
@@ -35,8 +93,9 @@ export default function PagePopup() {
                 <div className="flex flex-col">
                     <label className="label">Top Text *</label>
                     <input
+                        className={error.topText ? 'invalid' : ''}
                         type="text"
-                        name="name"
+                        name="topText"
                         value={formData.topText}
                         onChange={handleChange}
                     />
@@ -44,11 +103,68 @@ export default function PagePopup() {
                 <div className="flex flex-col">
                     <label className="label">Bottom Text *</label>
                     <input
+                        className={error.bottomText ? 'invalid' : ''}
                         type="text"
-                        name="name"
+                        name="bottomText"
                         value={formData.bottomText}
                         onChange={handleChange}
                     />
+                </div>
+                <div className="flex flex-col gap-6">
+                    <label className="label">List *</label>
+                    <ul className="flex flex-col gap-6">
+                        {list.map((item, index) => (
+                            <li
+                                className="flex flex-row items-center body-medium-regular text-black"
+                                key={item.bold + item.regular}>
+                                <div className="body-medium-bold mr-6 mt-2">{index + 1}.</div>
+                                <div>
+                                    <strong className="mr-6">{item.bold}</strong>
+                                    {item.regular}
+                                </div>
+                                <button
+                                    type="button"
+                                    className="forms-hint border rounded-2 ml-6 border-light-grey p-6 bg-lighter-grey"
+                                    onClick={() => removeItem(index)}>
+                                    Delete
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                    <input
+                        className={error.bold ? 'invalid' : ''}
+                        placeholder="Bold text *"
+                        type="text"
+                        name="bold"
+                        value={formData.bold}
+                        onChange={handleChange}
+                    />
+                    <input
+                        className={error.regular ? 'invalid' : ''}
+                        placeholder="Regular text *"
+                        type="text"
+                        name="regular"
+                        value={formData.regular}
+                        onChange={handleChange}
+                    />
+                    <button
+                        disabled={!formData.bold || !formData.regular}
+                        type="button"
+                        className="button-large border rounded-2 border-light-grey w-full py-6 bg-lighter-grey"
+                        onClick={addItem}>
+                        Add Item
+                    </button>
+                </div>
+                <div className="flex flex-row gap-12 w-full">
+                    <button type="button" className="secondary-button w-full">
+                        Cancel
+                    </button>
+                    <button
+                        disabled={disabledSaveButton()}
+                        type="submit"
+                        className="primary-button w-full">
+                        Save
+                    </button>
                 </div>
             </form>
         </div>

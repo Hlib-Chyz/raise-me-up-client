1ca381a497460ba712e5dff480102066209d99a9
GlebChiz <gchyz@jsdaddy.io>
2024-04-29 16:45:28 +0300

    feat: meta tags were added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import StoreProvider from './StoreProvider';
 import './globals.css';
 import { Toaster } from 'react-hot-toast';
+import Head from 'next/head';
 
 const openSans = Open_Sans({
     subsets: ['latin'],
@@ -22,6 +23,14 @@ export default function RootLayout({
 }>): React.JSX.Element {
     return (
         <html lang="en" className={`h-full ${openSans.className}`}>
+            <Head>
+                <title>Review 2024</title>
+                <meta name="description" content="PET project for review" />
+                <meta name="keywords" content="review" />
+                <meta property="og:title" content="Review 2024" />
+                <meta property="og:description" content="PET project for review" />
+                <meta property="og:type" content="website" />
+            </Head>
             <StoreProvider>
                 <body>
                     <Toaster position="top-center" />
